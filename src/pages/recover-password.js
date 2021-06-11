dba811ab67d6b30f1b34b09c9092a8468ec78da0
Wdark <wdark@iMac.local>
2021-06-11 10:21:44 -0500

    update recover passowrd

--- src/pages/recover-password.js
+++ src/pages/recover-password.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import SEO from "../components/seo"
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import { Form } from "react-bootstrap"
 import { Link } from "gatsby"
 
