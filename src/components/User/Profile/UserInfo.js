2b7edc314d84b27052809b594aa2012f4a7833c6
Nelson Alvarez <develop02@webdigitalark.com>
2021-06-15 17:52:56 -0500

    Styles text principal banner and modification ultimate component QR

--- src/components/User/Profile/UserInfo.js
+++ src/components/User/Profile/UserInfo.js
@@ -122,6 +122,8 @@ const UserInfo = (props) => {
                     <div className="title">
                       <h5 className="text-rotate">{props.packs[step].pack_title_lateral}</h5>
                       <h2 className="font-line-orange">{props.packs[step].pack_title}</h2>
+                      <h2 className="font-line-orange d-none d-lg-block">{props.packs[step].pack_title}</h2>
+                      <h2 className="font-line-orange d-lg-none">{props.packs[step].products[k].quantity}</h2>
                       <h2 className="third-text">pack</h2>
                     </div>
                     <div className="units">
