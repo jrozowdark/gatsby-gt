7d3fb540af572706a6d338a398c8c49c5806939d
Wdark <wdark@iMac.local>
2021-07-02 09:34:53 -0500

    git add changes for redemption

--- src/components/drupal-oauth/RedemptionForm.js
+++ src/components/drupal-oauth/RedemptionForm.js
@@ -51,19 +51,28 @@ class RedemptionForm extends React.Component {
         return true;
     })
     try {
+      this.setState({
+        processing: true
+      });
       await this.props.drupalOauthClient.handleSendRedemption(dataSend).then(data => {
         if(data.message){
+          console.log(data)
           this.setState({
             processing: false,
             error: data.message,
           });
+        }else{
+          navigate(data);
+          localStorage.setItem("product-redemption",JSON.stringify(sendLocal))
+          this.setState({
+            error: ''
+          });
         }
-        localStorage.setItem("product-redemption",JSON.stringify(sendLocal))
-        navigate(data);
+        this.setState({
+          processing: false
+        });
       });
-      this.setState({
-        processing: true
-      });
+
     } catch (err) {
       this.setState({
         processing: false,
@@ -77,6 +86,7 @@ class RedemptionForm extends React.Component {
       if(this.state.input !== null){
         let redem = this.props.redem;
         let {PackGroup,input} = this.state
+        const { error, processing } = this.state;
         return (
           <>
           <div className="box-zone">
@@ -111,11 +121,14 @@ class RedemptionForm extends React.Component {
                 </Form.Label>
                 )
               })}
-
               </Form.Group>
+              { error && !processing && <div className="text-error">{error} </div>}
+              {processing ?
+              <div className="loader">Realizando tú solicitud ...</div>:
               <div className="link button-first">
                 <input type="submit" value="Reclamar" onClick={ event => this.handleSubmit(event)} />
               </div>
+              }
             </form>
           </div>
             </>
