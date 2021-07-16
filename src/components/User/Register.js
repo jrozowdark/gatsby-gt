263af4915e95195075c502610e210e2603cdddc2
Wdark <wdark@iMac.local>
2021-07-16 06:36:38 -0500

    update Register

--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -1,11 +1,37 @@
 import React from "react"
 import RegisterForm from "../drupal-oauth/RegisterForm";
+import { navigate } from 'gatsby';
 class Register extends React.Component {
   state = {
-    username: ``,
-    password: ``,
+    code: ``,
   }
+  componentDidMount(){
+      const token = undefined;
+      const params = new URLSearchParams(this.props.location.search);
+      const code_data = params.get("code");
+      console.log(this.props);
+      console.log(params)
+      console.log(code_data)
+       if (code_data !== null) {
+        const service = fetch(`${process.env.GATSBY_DRUPAL_ROOT}/service/user/code`, {
+          method: 'POST',
+          headers: new Headers({
+            'Content-Type': 'application/json',
+          }),
+          body: JSON.stringify({"code" : code_data})
+        }).then(response => response.json())
+        .then(json => {
+          console.log(json)
+          if(json.error === false){
+            this.setState({access: false})
+            navigate('/user/login')
+          }else{
+          this.setState({access: true,code: code_data})
+          }
 
+        }).catch(error => console.log(error));
+       }
+  }
   render() {
     return (
       <div className="single-component">
@@ -14,7 +40,7 @@ class Register extends React.Component {
             <h2>registro</h2>
           </div>
           <div className="form-register">
-            <RegisterForm/>
+            <RegisterForm code ={this.state.code}/>
           </div>
         </div>
       </div>
